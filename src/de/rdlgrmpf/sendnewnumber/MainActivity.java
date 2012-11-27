feadf440b763a53f0419ceea547a5930209e47c0
Robin Morawetz <robin.morawetz@googlemail.com>
2012-11-27 21:49:50 +0100

    Basic contact retrieval
    retrieve contacts from database and put in a string <- optimize!
    
    http://stackoverflow.com/questions/2356084/read-all-contacts-phone-numbers-in-android

--- src/de/rdlgrmpf/sendnewnumber/MainActivity.java
+++ src/de/rdlgrmpf/sendnewnumber/MainActivity.java
@@ -1,22 +1,31 @@
 package de.rdlgrmpf.sendnewnumber;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
 	Button buttonNext;
+	TextView contactsTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		contactsTextView = (TextView) findViewById(R.id.contacts_text_view);
+		contactsTextView.setText(getList());
 		
 		buttonNext = (Button) findViewById(R.id.button_next);
 		
@@ -53,6 +62,27 @@ public class MainActivity extends Activity {
 		final Intent intent = new Intent(this, SendActivity.class);
 		startActivity(intent);
 	}
+	//End
 	
+	private String getList(){
+		String s = "";
+		
+		Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+		String[] projection = null;
+		String selection = null;
+		String[] selectionArgs = null;
+		String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME;
+		
+		Cursor phones = getContentResolver().query(uri, projection, selection, selectionArgs, sortOrder);
+		while (phones.moveToNext())
+		{
+		  String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+		  String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+		  s = s + name + "  " + phoneNumber + "\n";
+		}
+		phones.close();
+		
+		return s;
+	}
 
 }
